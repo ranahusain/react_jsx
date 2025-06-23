eb484fa265f118e5b4ff0dc7df371d3b6a14177f
Husain <capthussain2point0@gmail.com>
2025-06-23 15:59:17 +0500

    Nav Links change to active and noactive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,52 @@
 import logo from "../assets/images/logo.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+//use this for active links in navbars
+import { NavLink } from "react-router-dom";
+
 // now change a => link and href => to
 
 const Navbar = () => {
+  const linkclass = ({ isActive }) =>
+    isActive
+      ? " text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : " text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 ";
+
   return (
     <>
       <nav className="bg-indigo-700 border-b border-indigo-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-              {/* <!-- Logo --> */}
-              <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+              {/* <!-- Logo --> Commenting the below link for Comparison*/}
+              <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
                 <img className="h-10 w-auto" src={logo} alt="React Jobs" />
                 <span className="hidden md:block text-white text-2xl font-bold ml-2">
                   React Jobs
                 </span>
-              </Link>
-              <div className="md:ml-auto">
+              </NavLink>
+
+              {/* <div className="md:ml-auto">
                 <div className="flex space-x-2">
-                  <Link
+                  <NavLink
                     to="/"
                     className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Home
-                  </Link>
-                  <Link
-                    to="/jobs"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
+                  </NavLink> */}
+
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <NavLink to="/" className={linkclass}>
+                    Home
+                  </NavLink>
+
+                  <NavLink to="/jobs" className={linkclass}>
                     Jobs
-                  </Link>
-                  <Link
-                    to="/add-job"
-                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
+                  </NavLink>
+                  <NavLink to="/add-job" className={linkclass}>
                     Add Job
-                  </Link>
+                  </NavLink>
                 </div>
               </div>
             </div>
