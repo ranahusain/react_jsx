f55e78bc9da6b36f098a4c0bc6a0718fe2974806
Husain <capthussain2point0@gmail.com>
2025-06-23 15:43:22 +0500

    404 page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
-
 import MainLayout from "./layouts/MainLayout";
-
+import NotFoundPage from "./pages/NotFoundPage";
 // import Navbar from "./components/Navbar";
 // import Hero from "./components/Hero";
 // import HomeCards from "./components/HomeCards";
@@ -21,6 +20,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
