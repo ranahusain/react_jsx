dd76c02140971ced92091b4b81a04402d7da79e3
Husain <capthussain2point0@gmail.com>
2025-06-24 17:24:49 +0500

    states add for form

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,7 +1,6 @@
 // import { useState, useEffect } from "react";
 // import { useParams } from "react-router-dom";
 // import Spinner from "../components/Spinner";
-// import { HiH1 } from "react-icons/hi2";
 // const JobPage = () => {
 //   const [job, setJob] = useState([]);
 //   // useEffect(()=>{},[]) basic structure of useEffect
